ef0b2b9925dd554f0e1d3540c51d0f0f24f95c55
Ally Chen <allyroseline@gmail.com>
2021-11-22 16:29:17 +0800

    add colors

---
+++ src/const.js
@@ -0,0 +1,11 @@
+export const colors = {
+  main: '#0077FF',
+  secondary: '#FFC44E',
+  dark: '#1E222B',
+  tint: '#99BADD',
+  light: '#486AE8',
+  textDark: '#363636',
+  textMedium: '#7D7D7D',
+  textLight: '#B6B6B6',
+  textLighter: '#D9D9D9',
+};
